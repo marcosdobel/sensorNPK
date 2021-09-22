967cab44b61ccf5199ac0497ba91e79af668855c
Marco <antonio_masg@outlook.com>
2021-09-21 21:41:38 -0500

    Primer Prueba
    Aqui esta el primer codigo de los sensores NPK

---
+++ Prueba1.ino
@@ -0,0 +1,98 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>
+
+
+#define RE 8
+#define DE 7
+ 
+//const byte code[]= {0x01, 0x03, 0x00, 0x1e, 0x00, 0x03, 0x65, 0xCD};
+const byte nitro[] = {0x01,0x03, 0x00, 0x1e, 0x00, 0x01, 0xe4, 0x0c};
+const byte phos[] = {0x01,0x03, 0x00, 0x1f, 0x00, 0x01, 0xb5, 0xcc};
+const byte pota[] = {0x01,0x03, 0x00, 0x20, 0x00, 0x01, 0x85, 0xc0};
+ 
+byte values[11];
+SoftwareSerial mod(2,3);
+ 
+void setup() {
+  Serial.begin(9600);
+  mod.begin(9600);
+  pinMode(RE, OUTPUT);
+  pinMode(DE, OUTPUT);
+  
+
+}
+ 
+void loop() {
+  byte val1,val2,val3;
+  val1 = nitrogeno();
+  delay(250);
+  val2 = fosforo();
+  delay(250);
+  val3 = potasio();
+  delay(250);
+  
+  
+  Serial.print("Nitrogeno: ");
+  Serial.print(val1);
+  Serial.println(" mg/kg");
+  Serial.print("Fosforo: ");
+  Serial.print(val2);
+  Serial.println(" mg/kg");
+  Serial.print("Potasio: ");
+  Serial.print(val3);
+  Serial.println(" mg/kg");
+  delay(2000);
+ 
+
+}
+ 
+byte nitrogeno(){
+  digitalWrite(DE,HIGH);
+  digitalWrite(RE,HIGH);
+  delay(10);
+  if(mod.write(nitro,sizeof(nitro))==8){
+    digitalWrite(DE,LOW);
+    digitalWrite(RE,LOW);
+    for(byte i=0;i<7;i++){
+    //Serial.print(mod.read(),HEX);
+    values[i] = mod.read();
+    Serial.print(values[i],HEX);
+    }
+    Serial.println();
+  }
+  return values[4];
+}
+ 
+byte fosforo(){
+  digitalWrite(DE,HIGH);
+  digitalWrite(RE,HIGH);
+  delay(10);
+  if(mod.write(phos,sizeof(phos))==8){
+    digitalWrite(DE,LOW);
+    digitalWrite(RE,LOW);
+    for(byte i=0;i<7;i++){
+    //Serial.print(mod.read(),HEX);
+    values[i] = mod.read();
+    Serial.print(values[i],HEX);
+    }
+    Serial.println();
+  }
+  return values[4];
+}
+ 
+byte potasio(){
+  digitalWrite(DE,HIGH);
+  digitalWrite(RE,HIGH);
+  delay(10);
+  if(mod.write(pota,sizeof(pota))==8){
+    digitalWrite(DE,LOW);
+    digitalWrite(RE,LOW);
+    for(byte i=0;i<7;i++){
+    //Serial.print(mod.read(),HEX);
+    values[i] = mod.read();
+    Serial.print(values[i],HEX);
+    }
+    Serial.println();
+  }
+  return values[4];
+}
